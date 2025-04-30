5ce51d7f2eeafae93157a7c7944ffcd2de47c488
klonqdoublek <ipichaya.2004@gmail.com>
2025-04-30 14:30:24 +0700

    Very Last Final

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,32 @@
-import React from 'react'
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <div className="mb-16">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-linear-65 from-purple-500 to-secondary-red">
+      <div className="flex flex-col items-center bg-white shadow-md rounded-xl pl-16 pr-16 pt-8 pb-8"> 
+      <div className="mb-12 mt-12">
         <Image 
           src="/Dorm.life.svg" 
           alt="Dorm Life Logo"
           width={200}
-          height={100}
-          priority
+          height={150}
         />
       </div>
-
       <div className="text-center mb-8">
-        <h1 className="text-4xl font-bold">
+        <h1 className="font-display text-3xl font-medium">
           Welcome to Dormlife
         </h1>
-        <h3 className="text-2xl p-2">
+        <h3 className="text-xl opacity-50">
           by klonqdevblek
         </h3>
       </div>
       <div>
         <Link href="/login">
-          <Button>Login</Button>
+          <Button>Get Started</Button>
         </Link>
+      </div>
       </div>
     </div>
   )
